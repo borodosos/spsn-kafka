b1646383b15072c41de0307e1c09a4fddbc9e24c
Georgiy Malahov <g.malahov@tg.dunice.net>
2024-11-15 18:12:00 +0300

    test

--- src/Console/Commands/SpsnKafkaConsumer.php
+++ src/Console/Commands/SpsnKafkaConsumer.php
@@ -5,7 +5,6 @@ namespace Spsn\Kafka\Console\Commands;
 use Illuminate\Console\Command;
 use Junges\Kafka\Contracts\KafkaMessage;
 use Junges\Kafka\Facades\Kafka;
-use Spsn\Kafka\Events\SpsnKafkaMessageReceived;
 
 class SpsnKafkaConsumer extends Command {
     /**
@@ -26,11 +25,12 @@ class SpsnKafkaConsumer extends Command {
      * Execute the console command.
      */
     public function handle() {
-        $consumer = Kafka::consumer(config('spsn_kafka.topics'), null, config('kafka.brokers'))
+        $consumer = Kafka::consumer([config('spsn_kafka.topics.spsn-notify')], null, config('kafka.brokers'))
             ->withOptions(config('spsn_kafka.consumer.options'))
+            ->withConsumerGroupId(1)
             ->withHandler(function (KafkaMessage $message) {
                 $this->info('Kafka message: ' . $message->getBody());
-                event(new SpsnKafkaMessageReceived($message->getKey(), $message->getBody(), $message->getTopicName(), $message->getHeaders()));
+                // event(new SpsnKafkaMessageReceived($message->getKey(), $message->getBody(), $message->getTopicName(), $message->getHeaders()));
             })
             ->build();
 
