61ac4c5127a3f37f5e38a2c52c50a0a403bc3fe8
Georgiy Malahov <g.malahov@tg.dunice.net>
2024-11-19 13:57:58 +0300

    update

--- src/Constants/MessageTypes/SpsnNotifySrv/SpsnNotifyMessageTypes.php
+++ src/Constants/MessageTypes/SpsnNotifySrv/SpsnNotifyMessageTypes.php
@@ -5,4 +5,5 @@ namespace Spsn\Kafka\Constants\MessageTypes\SpsnNotifySrv;
 class SpsnNotifyMessageTypes {
     public const VERIFY_EMAIL = 'verify_email';
     public const SEND_SMS = 'send_sms';
+    public const FORGOT_PASSWORD = 'forgot_password';
 }
