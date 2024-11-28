620ffed31cd0c4f170c519ef74e8d396f55609d0
Georgiy Malahov <g.malahov@tg.dunice.net>
2024-11-28 16:55:09 +0300

    add consts

---
+++ src/Constants/SpsnServiceInvitaionCodes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Spsn\Kafka\Constants;
+
+class SpsnServiceInvitaionCodes {
+    public const RECEIVED_ON_CLIENT_SIDE = '100201';
+    public const INVITATION_ACCEPTED = '100205';
+    public const USER_DATA_CHANGED = '100206';
+    public const INVITATION_REJECTED = '100505';
+}
