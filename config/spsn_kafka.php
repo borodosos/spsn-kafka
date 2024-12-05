a6a0c21b9d78b50796f0a344c15d7c025470fc5f
Georgiy Malahov <g.malahov@tg.dunice.net>
2024-12-05 16:55:18 +0300

    update config

--- config/spsn_kafka.php
+++ config/spsn_kafka.php
@@ -80,15 +80,6 @@ return [
             SpsnAppServiceNames::SPSN_STT => 'kuser-td-stage-stt-srv',
             SpsnAppServiceNames::SPSN_LOG => 'kuser-td-stage-log-srv',
         ],
-        'dev_custom' => [
-            SpsnAppServiceNames::SPSN_TD => 'kuser-td-stage-td-srv',
-            SpsnAppServiceNames::SPSN_NOTIFY => 'kuser-td-stage-notif-srv',
-            SpsnAppServiceNames::SPSN_BILLING => 'kuser-td-stage-billing-srv',
-            SpsnAppServiceNames::SPSN_TIKETS => 'kuser-td-stage-tiket-srv',
-            SpsnAppServiceNames::SPSN_IDOCS => 'kuser-td-stage-idocs-srv',
-            SpsnAppServiceNames::SPSN_STT => 'kuser-td-stage-stt-srv',
-            SpsnAppServiceNames::SPSN_LOG => 'kuser-td-stage-log-srv',
-        ],
         default => [
             SpsnAppServiceNames::SPSN_TD => 'kuser-td-dev-td-srv',
             SpsnAppServiceNames::SPSN_NOTIFY => 'kuser-td-dev-notif-srv',
