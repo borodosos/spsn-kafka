b8cbbb341138e0185b4698dc45bb05ae86596b69
Georgy Malahov <stepllerrus78@gmail.com>
2025-02-03 13:35:26 +0300

    FEATURE: add new constants

---
+++ src/Constants/MessageTypes/SpsnIDocsSrv/SpsnIDocsMessageTypes.php
@@ -0,0 +1,9 @@
+<?php
+namespace Spsn\Kafka\Constants\MessageTypes\SpsnIDocsSrv;
+
+class SpsnIDocsMessageTypes {
+    public const SERVICE_DOCUMENT = 'service_document';
+    public const CONTENT          = 'content';
+    public const SERVICE_USER     = 'service_user';
+    public const INVITATION       = 'invitation';
+}
