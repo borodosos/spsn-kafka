fa651a49f59c14c9828ad46831fa4e1ecddeb031
Georgy Malahov <stepllerrus78@gmail.com>
2025-02-03 16:32:42 +0300

    FEATURE: change transform method

--- src/Data/MainService/InvitationMessageDTO.php
+++ src/Data/MainService/InvitationMessageDTO.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Spsn\Kafka\Data\MainService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Str;
@@ -31,25 +30,31 @@ class InvitationMessageDTO extends Data {
 
     public function transform(null | TransformationContextFactory | TransformationContext $transformationContext = null): array {
         return [
-            'message_id' => $this->message_id ?? Str::uuid(),
-            "message_type" => $this->message_type,
-            "sender" => [
-                "id" => $this->sender->id,
-                "country_code" => $this->sender->country_code,
-                "company" => $this->sender->company,
-                "inn" => $this->sender->inn,
-                "kpp" => $this->sender->kpp,
-                "bin" => $this->sender->bin,
-                "email" => $this->sender->email,
+            'message_id'         => $this->message_id ?? Str::uuid(),
+            "message_type"       => $this->message_type,
+            'sender_operator'    => [
+                'id' => $this->sender_operator->id,
             ],
-            "recipient" => [
-                "id" => $this->recipient->id,
+            'recipient_operator' => [
+                'id' => $this->recipient_operator->id,
+            ],
+            "sender"             => [
+                "id"           => $this->sender->id,
+                "country_code" => $this->sender->country_code,
+                "company"      => $this->sender->company,
+                "inn"          => $this->sender->inn,
+                "kpp"          => $this->sender->kpp,
+                "bin"          => $this->sender->bin,
+                "email"        => $this->sender->email,
+            ],
+            "recipient"          => [
+                "id"           => $this->recipient->id,
                 "country_code" => $this->recipient->country_code,
-                "company" => $this->recipient->company,
-                "inn" => $this->recipient->inn,
-                "kpp" => $this->recipient->kpp,
-                "bin" => $this->recipient->bin,
-                "email" => $this->recipient->email,
+                "company"      => $this->recipient->company,
+                "inn"          => $this->recipient->inn,
+                "kpp"          => $this->recipient->kpp,
+                "bin"          => $this->recipient->bin,
+                "email"        => $this->recipient->email,
             ],
         ];
     }
