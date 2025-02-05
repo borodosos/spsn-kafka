9de1d8c538396aa6b7ed59d8da12e82b6542e6d5
Georgy Malahov <stepllerrus78@gmail.com>
2025-02-05 14:27:34 +0300

    FEATURE: add new consts

--- src/Constants/MessageTypes/SpsnIDocsSrv/SpsnIDocsMessageTypes.php
+++ src/Constants/MessageTypes/SpsnIDocsSrv/SpsnIDocsMessageTypes.php
@@ -2,8 +2,9 @@
 namespace Spsn\Kafka\Constants\MessageTypes\SpsnIDocsSrv;
 
 class SpsnIDocsMessageTypes {
-    public const SERVICE_DOCUMENT = 'service_document';
-    public const CONTENT          = 'content';
-    public const SERVICE_USER     = 'service_user';
-    public const INVITATION       = 'invitation';
+    public const SERVICE_DOCUMENT   = 'service_document';
+    public const CONTENT            = 'content';
+    public const SERVICE_USER       = 'service_user';
+    public const INVITATION         = 'invitation';
+    public const SERVICE_INVITATION = 'service_invitation';
 }
--- src/Constants/MessageTypes/SpsnTdSrv/SpsnTdMessageTypes.php
+++ src/Constants/MessageTypes/SpsnTdSrv/SpsnTdMessageTypes.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace Spsn\Kafka\Constants\MessageTypes\SpsnTdSrv;
 
 class SpsnTdMessageTypes {
-    public const SERVICE_DOCUMENT = 'service_document';
-    public const CONTENT = 'content';
-    public const SERVICE_USER = 'service_user';
-    public const INVITATION = 'invitation';
+    public const SERVICE_DOCUMENT   = 'service_document';
+    public const CONTENT            = 'content';
+    public const SERVICE_USER       = 'service_user';
+    public const INVITATION         = 'invitation';
+    public const SERVICE_INVITATION = 'service_invitation';
 }
