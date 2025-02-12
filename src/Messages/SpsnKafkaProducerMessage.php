10c88820e74ae38ae06ca6739c35f82331211892
Georgy Malahov <stepllerrus78@gmail.com>
2025-02-12 11:56:01 +0300

    FEATURE: change producer message

--- src/Messages/SpsnKafkaProducerMessage.php
+++ src/Messages/SpsnKafkaProducerMessage.php
@@ -3,22 +3,27 @@ namespace Spsn\Kafka\Messages;
 
 use Carbon\Carbon;
 use Junges\Kafka\Message\Message;
+use Spsn\Kafka\Constants\SpsnAppServiceNames;
 use Str;
 
 class SpsnKafkaProducerMessage extends Message {
     public function __construct(
         string $messageType,
         mixed $message = null,
+        ?string $forAppService = null,
         ?array $headers = null,
         ?string $key = null
     ) {
         parent::__construct(
             headers: $headers ?? ['header-key' => 'key', 'id' => Str::uuid(), 'app_service' => config('spsn_kafka.app_service_name')],
-            body: [
-                'type'      => $messageType,
-                'timestamp' => Carbon::now()->format('d.m.Y H:i:s'),
-                'payload'   => $message,
-            ],
+            body: match ($forAppService) {
+                SpsnAppServiceNames::SPSN_IDOCS => $message,
+                default                         => [
+                    'type'      => $messageType,
+                    'timestamp' => Carbon::now()->format('d.m.Y H:i:s'),
+                    'payload'   => $message,
+                ]
+            },
             key: $key ?? 'key',
         );
 
