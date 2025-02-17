47fdf5bec0ba53f73b18999606d9289d89a73b7e
Georgy Malahov <stepllerrus78@gmail.com>
2025-02-18 00:57:55 +0300

    FEATURE: add new var

--- src/Data/MainService/InvitationServiceMessageDTO.php
+++ src/Data/MainService/InvitationServiceMessageDTO.php
@@ -16,6 +16,7 @@ class InvitationServiceMessageDTO extends Data {
         public RecipientDTO $recipient,
         public ServiceMessageDTO $service_message,
         public string | int | null $message_id = null,
+        public string | int | null $workflow_id = null,
     ) {
     }
 
@@ -30,34 +31,35 @@ class InvitationServiceMessageDTO extends Data {
 
     public function transform(null | TransformationContextFactory | TransformationContext $transformationContext = null): array {
         return [
-            'message_id' => $this->message_id ?? Str::uuid(),
-            "message_type" => $this->message_type,
-            'sender_operator' => [
+            'workflow_id'        => $this->workflow_id,
+            'message_id'         => $this->message_id ?? Str::uuid(),
+            "message_type"       => $this->message_type,
+            'sender_operator'    => [
                 'id' => $this->sender_operator->id,
             ],
             'recipient_operator' => [
                 'id' => $this->recipient_operator->id,
             ],
-            "sender" => [
-                "id" => $this->sender->id,
+            "sender"             => [
+                "id"           => $this->sender->id,
                 "country_code" => $this->sender->country_code,
-                "company" => $this->sender->company,
-                "inn" => $this->sender->inn,
-                "kpp" => $this->sender->kpp,
-                "bin" => $this->sender->bin,
-                "email" => $this->sender->email,
+                "company"      => $this->sender->company,
+                "inn"          => $this->sender->inn,
+                "kpp"          => $this->sender->kpp,
+                "bin"          => $this->sender->bin,
+                "email"        => $this->sender->email,
             ],
-            "recipient" => [
-                "id" => $this->recipient->id,
+            "recipient"          => [
+                "id"           => $this->recipient->id,
                 "country_code" => $this->recipient->country_code,
-                "company" => $this->recipient->company,
-                "inn" => $this->recipient->inn,
-                "kpp" => $this->recipient->kpp,
-                "bin" => $this->recipient->bin,
-                "email" => $this->recipient->email,
+                "company"      => $this->recipient->company,
+                "inn"          => $this->recipient->inn,
+                "kpp"          => $this->recipient->kpp,
+                "bin"          => $this->recipient->bin,
+                "email"        => $this->recipient->email,
             ],
-            "service_message" => [
-                "code" => $this->service_message->code,
+            "service_message"    => [
+                "code"    => $this->service_message->code,
                 "content" => $this->service_message->content,
                 "payload" => $this->service_message->payload,
             ],
