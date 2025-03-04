fb124b726dc37356590042294d99dfd224db9ea6
Georgy Malahov <stepllerrus78@gmail.com>
2025-03-04 15:08:32 +0300

    FEATURE: delete transform method

--- src/Data/NotifyService/NotifyMessageDTO.php
+++ src/Data/NotifyService/NotifyMessageDTO.php
@@ -1,9 +1,6 @@
 <?php
-
 namespace Spsn\Kafka\Data\NotifyService;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Support\Transformation\TransformationContext;
-use Spatie\LaravelData\Support\Transformation\TransformationContextFactory;
 
 class NotifyMessageDTO extends Data {
 
@@ -21,13 +18,5 @@ class NotifyMessageDTO extends Data {
         } catch (\Exception $e) {
             throw new \Exception($e->getMessage());
         }
-    }
-
-    public function transform(null | TransformationContextFactory | TransformationContext $transformationContext = null): array {
-        return [
-            'notify_type' => $this->notify_type,
-            'address' => $this->address,
-            'data' => $this->data,
-        ];
     }
 }
