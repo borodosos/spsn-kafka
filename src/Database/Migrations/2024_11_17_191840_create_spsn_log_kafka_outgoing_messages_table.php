a61a582f8af85ed01de01b1e626e160d172d3d0a
aav-railexpert <aav@railexpert.ru>
2025-03-24 15:49:11 +0300

    Fix table name in migrations

--- src/Database/Migrations/2024_11_17_191840_create_spsn_log_kafka_outgoing_messages_table.php
+++ src/Database/Migrations/2024_11_17_191840_create_spsn_log_kafka_outgoing_messages_table.php
@@ -25,6 +25,6 @@ return new class() extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('log_kafka_outgoing_messages');
+        Schema::dropIfExists('spsn_log_kafka_outgoing_messages');
     }
 };
