84ff237512d6b9ac70238304bd2059638be895d1
aav-railexpert <aav@railexpert.ru>
2025-09-22 10:58:57 +0300

    новый тип лога - error

--- src/Constants/MessageTypes/SpsnLogSrv/SpsnLogMessageTypes.php
+++ src/Constants/MessageTypes/SpsnLogSrv/SpsnLogMessageTypes.php
@@ -5,4 +5,5 @@ class SpsnLogMessageTypes {
     public const EXCEPTION = 'exception';
     public const KAFKA_HANDLER_EXCEPTION = 'kafka_handler_exception';
     public const INFO = 'info';
+    public const ERROR = 'error';
 }
